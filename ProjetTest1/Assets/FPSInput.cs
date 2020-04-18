0eedf51007358cc3dff1f10042f0b85bc4809318
thomas duvette <thomasduvette@gmail.com>
2020-04-18 17:25:54 +0200

    :sparkles: création de la maison

---
+++ ProjetTest1/Assets/FPSInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float gravity = -9.8f;
+
+    private CharacterController _charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        movement.y = gravity * Time.deltaTime;
+        _charController.Move(movement);
+    }
+}
